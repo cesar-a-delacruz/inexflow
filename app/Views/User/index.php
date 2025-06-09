acade1c17678411624381890baa50c547193cc56
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-06-09 18:20:37 -0500

    feat: pasar registro de tabla users al index

--- app/Views/User/index.php
+++ app/Views/User/index.php
@@ -8,19 +8,19 @@
 <div class="card">
     <div class="field">
         <p class="key">Nombre</p>
-        <p class="value"></p>
+        <p class="value"><?= $user->name ?></p>
     </div>
     <div class="field">
         <p class="key">Correo</p>
-        <p class="value"></p>
+        <p class="value"><?= $user->email ?></p>
     </div>
     <div class="field">
         <p class="key">Estado</p>
-        <p class="value"></p>
+        <p class="value"><?= $user->is_active == true ? 'Activo' : 'Inactivo' ?></p>
     </div>
     <div class="field">
         <p class="key">Rol</p>
-        <p class="value"></p>
+        <p class="value"><?= $user->role ?></p>
     </div>
 </div>
 <?= $this->endSection() ?>
