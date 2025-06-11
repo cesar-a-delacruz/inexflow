06064d724c10a935a9fa59e081f63ee4b167d123
ShadowDarks25Y <47618971+ShadowDarks25Y@users.noreply.github.com>
2025-06-10 22:08:08 -0500

    feat: Añadir BusinessSeeder

---
+++ app/Database/Seeds/BusinessSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class BusinessSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            [
+                'business_name' => 'business a',
+                'owner_name' => 'markus diaz',
+                'owner_email' => 'markdi01@gmail.com',
+                'owner_phone' => '66588901',
+                'tax_id' => 'Emxksfvmfa12',
+                'address' => 'Changuinola',
+                'logo_url' => 'https://1000marcas.net/wp-content/uploads/2019/11/Instagram-Logo.jpg',
+                'onboarding_completed' => false,
+                'status' => 'active',
+                'registered_by'=> 1,
+            ],
+            [
+                'business_name' => 'business b',
+                'owner_name' => 'michael jackson',
+                'owner_email' => 'mich07@gmail.com',
+                'owner_phone' => '65809012',
+                'tax_id' => 'Bvgkhyvmdu36',
+                'address' => 'Las Tablas',
+                'logo_url' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/2048px-LEGO_logo.svg.png',
+                'onboarding_completed' => false,
+                'status' => 'active',
+                'registered_by'=> 1,
+            ],
+        ];
+        $this->db->table('businesses')->insertBatch($data);
+    }
+}
