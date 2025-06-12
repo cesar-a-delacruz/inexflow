9408186dd2c38150d1389090159c8150abcbae62
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-06-12 14:13:36 -0500

    fix: remover campos innecesarios en BusinessSeeder

--- app/Database/Seeds/BusinessSeeder.php
+++ app/Database/Seeds/BusinessSeeder.php
@@ -19,10 +19,6 @@ class BusinessSeeder extends Seeder
             'owner_name' => 'markus diaz',
             'owner_email' => 'markdi01@gmail.com',
             'owner_phone' => '66588901',
-            'tax_id' => 'Emxksfvmfa12',
-            'address' => 'Changuinola',
-            'logo_url' => 'https://1000marcas.net/wp-content/uploads/2019/11/Instagram-Logo.jpg',
-            'status' => 'active',
             'registered_by'=> Uuid::uuid3(Uuid::NAMESPACE_URL, '1'),
         ]));
         $model->createBusiness(new Business([
@@ -31,10 +27,6 @@ class BusinessSeeder extends Seeder
             'owner_name' => 'michael jackson',
             'owner_email' => 'mich07@gmail.com',
             'owner_phone' => '65809012',
-            'tax_id' => 'Bvgkhyvmdu36',
-            'address' => 'Las Tablas',
-            'logo_url' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/2048px-LEGO_logo.svg.png',
-            'onboarding_completed' => false,
             'status' => 'active',
             'registered_by'=> Uuid::uuid3(Uuid::NAMESPACE_URL, '1'),
         ]));
