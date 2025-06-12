84f5cc0f6fbf395184eca23af5d79996e27240ea
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-06-12 10:26:49 -0500

    fix: agregar placeholder personalizado para usuario

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,8 +5,10 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/user/(:num)', 'UserController::show/$1');
-$routes->put('/user/(:num)', 'UserController::update/$1');
-$routes->get('/user/new', 'UserController::new');
+$routes->addPlaceholder('uuid', '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}');
+$routes->get('/user/(:uuid)', 'UserController::show/$1');
+$routes->put('/user/(:uuid)', 'UserController::update/$1');
+$routes->get('/user/new/', 'UserController::new');
 $routes->get('/', 'UserController::login');
+$routes->post('/', 'UserController::verify');
 $routes->get('user/index', 'UserController::index');
