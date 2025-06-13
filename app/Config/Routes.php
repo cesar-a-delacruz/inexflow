425654c831d48a4f3597715fd06a185a5a1cff6d
TrotmanCH <charlytr207@gmail.com>
2025-06-13 00:07:21 -0500

    Se agregan los controladores de Login y Dashboard, tambien las vista de Login y el dashboard de Admin

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,10 +11,18 @@ $routes->get('user/(:uuid)', 'UserController::show/$1');
 $routes->put('user/(:uuid)', 'UserController::update/$1');
 $routes->get('user/new', 'UserController::new');
 $routes->post('user/', 'UserController::create');
-$routes->get('/', 'UserController::login');
+
 $routes->get('user/', 'UserController::index');
 // Rutas del negocio
 $routes->get('user/(:uuid)/business/new', 'BusinessController::new/$1');
 $routes->post('user/(:uuid)', 'BusinessController::create');
 $routes->get('user/(:uuid)/business', 'BusinessController::show/$1');
 $routes->put('user/(:uuid)/business', 'BusinessController::update/$1');
+
+//Rutas de Login
+$routes->get('/','LoginController::index');
+$routes->get('login','LoginController::index');
+$routes->post('login','LoginController::procesarLogin');
+$routes->get('logout', 'LoginController::logout'); 
+
+$routes->get('dashboard/admin', 'DashboardController::index');
