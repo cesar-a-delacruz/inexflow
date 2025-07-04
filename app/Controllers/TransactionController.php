ac10e94efa93e4fb83003aafcff378bd9a8f1b22
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-07-04 04:07:14 -0500

    feat: restringir acceso a las transacciones

--- app/Controllers/TransactionController.php
+++ app/Controllers/TransactionController.php
@@ -10,9 +10,17 @@ class TransactionController extends BaseController
   protected TransactionsModel $model;
   public function __construct() {
     $this->model = new TransactionsModel();
+    helper('session');
   }
+  
   public function index()
     {
+      $current_page = session()->get('current_page');
+      if (is_admin() && $current_page) return redirect()->to($current_page);
+
+      if (!user_logged()) return redirect()->to('/');
+      else session()->set('current_page', 'transactions');
+
       $data['title'] = 'Transacciones';
       $data['transactions'] = $this->model->findAllWithCategory();
       return view('Transaction/index', $data);
