6d980382d45fc8c6806d2a6308a6e1512ecc4c6d
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-07-04 00:16:55 -0500

    fix: mostrar datos de transactions en en la vista index

--- app/Controllers/TransactionController.php
+++ app/Controllers/TransactionController.php
@@ -3,12 +3,18 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\TransactionsModel;
 
 class TransactionController extends BaseController
 {
+  protected TransactionsModel $model;
+  public function __construct() {
+    $this->model = new TransactionsModel();
+  }
   public function index()
     {
       $data['title'] = 'Transacciones';
+      $data['transactions'] = $this->model->findAll();
       return view('Transaction/index', $data);
     }
 }
