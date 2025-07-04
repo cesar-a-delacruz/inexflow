714963c8b654624c0d128ea040d4aee7b3a9e184
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-07-04 04:05:20 -0500

    refactor: mover el acceso de categorias y transacciones

--- app/Views/Transaction/index.php
+++ app/Views/Transaction/index.php
@@ -4,7 +4,7 @@
 <div class="container-fluid">
   <h1 class="mb-4"><?= $title ?></h1>
   <button type="button" class="btn btn-primary">Registrar transacción</button>
-
+  <a href="/categories" class="btn btn-success">Ver Categorías</a>
   <div class="table-responsive" >
     <table id="showtable" class="table table-striped table-hover table-bordered">
       <thead class="table-dark">
--- app/Views/layouts/dashboard.php
+++ app/Views/layouts/dashboard.php
@@ -61,7 +61,7 @@
                     <line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line>
                     <line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3.01" y2="6"></line>
                     <line x1="3" y1="12" x2="3.01" y2="12"></line><line x1="3" y1="18" x2="3.01" y2="18"></line>
-                </svg>
+                  </svg>
                   Usuarios
                 </a>
               </li>
@@ -70,16 +70,22 @@
                   <a class="nav-link" href="/business" role="button">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" 
                     stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-briefcase">
-                    <rect x="2" y="7" width="20" height="14" rx="2" ry="2"></rect>
-                    <path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path></svg>
+                      <rect x="2" y="7" width="20" height="14" rx="2" ry="2"></rect>
+                      <path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
+                    </svg>
                     Negocio
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="/categories" role="button">
-                    Categorías
+                  <a class="nav-link" href="/transactions" role="button">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" 
+                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-dollar-sign">
+                      <line x1="12" y1="1" x2="12" y2="23"></line>
+                      <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
+                    </svg>
+                    Transacciones
                   </a>
-              </li>
+                </li>
               <?php endif; ?>
           </ul>
         </div>
