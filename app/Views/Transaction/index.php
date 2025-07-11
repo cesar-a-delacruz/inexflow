dfe35b1f8f4e445d64f1b8b0a81248dd3c5ac41c
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-07-11 00:50:44 -0500

    fix: corregir error en link a transaccion

--- app/Views/Transaction/index.php
+++ app/Views/Transaction/index.php
@@ -32,7 +32,7 @@
               <td><?= $transactions[$i]->notes ?></td>
               <td>
                 <div class="btn-group" role="group">
-                  <a href="/transaction/<?= $i+1 ?>" class="btn btn-success btn-sm">
+                  <a href="/transaction/<?= $transactions[$i]->id ?>" class="btn btn-success btn-sm">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" 
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit">
                       <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
