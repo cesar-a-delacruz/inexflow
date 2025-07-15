fcd5c2516892b71f4bae6c8991e3d8c61c3ada1d
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-07-15 14:41:25 -0500

    refactor: reduciendo helper de session

--- app/Helpers/session_helper.php
+++ app/Helpers/session_helper.php
@@ -1,11 +1,9 @@
 <?php
-if (!function_exists('user_logged')) {
-    function user_logged() {
-        return session()->get('id') !== null;
-    }
-}
-if (!function_exists('is_admin')) {
-    function is_admin() {
-        return session()->get('role') === 'admin';
+if (!function_exists('check_user')) {
+    function check_user($page_role) {
+        if(!session()->get('id')) return '/';
+        if (session()->get('role') !== $page_role) return session()->get('current_page');
+
+        return null;
     }
 }
